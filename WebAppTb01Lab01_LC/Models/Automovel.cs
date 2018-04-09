b3b4aa715fa4c986aa140334eaef1116e6f0b92c
luciano silva da costa maninho <luciano.silvadacosta@outlook.com>
2018-04-09 15:25:17 -0400

    Add project files.

---
+++ WebAppTb01Lab01_LC/Models/Automovel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTb01Lab01_LC.Models
+{
+    public class Automovel
+    {
+        //Propriedade(Primary key) - PK
+        public int AutomovelId { get; set; }
+
+        [Required(ErrorMessage = "Tipo é obrigatório")]
+        public Tipo Tipo { get; set; }
+
+        [Required(ErrorMessage = "Descrição é obrigatório")]
+        [StringLength(300, ErrorMessage = "Máximo 300 caracteres")]
+        public String Descricao { get; set; }
+
+        [Required(ErrorMessage = "Marca é obrigatório")]
+        [StringLength(300, ErrorMessage = "Máximo 300 caracteres")]
+        public String Marca { get; set; }
+
+        [Required(ErrorMessage = "Disponível é obrigatório")]
+        public bool Disponivel { get; set; }
+
+        [Required(ErrorMessage = "Data de cadastro é obrigatório")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
